b96d3aa57721ecfc6a9aac1f21f2e7731b2123e9
Liam <liamrumpypumpy@protonmail.com>
2023-03-13 19:24:00 +0000

    chore: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+     <h2 className="text-2xl">Hello world</h2>
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
