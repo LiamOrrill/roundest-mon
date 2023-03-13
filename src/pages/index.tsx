020d165f2c27fe59664fc021ec1a8d1c269ce522
Liam <liamrumpypumpy@protonmail.com>
2023-03-13 19:44:43 +0000

    chore: basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
-    <>
-     <h2 className="text-2xl">Hello world</h2>
-    </>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl">Which Pokemon is Rounder?</div>
+      <div className="p-2" />
+      <div className="flex max-w-2xl items-center justify-between rounded border p-8">
+        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="p-8">vs</div>
+        <div className="h-16 w-16 bg-red-200"></div>
+      </div>
+    </div>
   );
 };
 
